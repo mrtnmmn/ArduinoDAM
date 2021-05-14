1d1da07f5f90c9a5528fbffbdc5dfe6c8ca5cb45
mrtnmmn <72600574+mrtnmmn@users.noreply.github.com>
2021-05-14 20:26:51 +0200

    Add files via upload

---
+++ Ej08/Ej08.ino
@@ -0,0 +1,12 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(13, 12, 5, 4, 3, 2);
+
+void setup() {
+	 lcd.begin(16, 2);
+}
+
+void loop() {
+	lcd.setCursor(0,0);
+    lcd.print("Hola mundo!");
+}
